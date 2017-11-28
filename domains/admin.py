bfa98eeff865ca539f1fcb3bcde213a20f0dc466
Alex Conrey <alex@conrey.us>
2017-11-28 00:11:39 -0600

    added list_display for domainadmin model

--- domains/admin.py
+++ domains/admin.py
@@ -5,8 +5,11 @@ from django.contrib import admin
 
 from .models import Domain, RecordType, Record, Nameserver
 
+class DomainAdmin(admin.ModelAdmin):
+	model = Domain
+	list_display = ['name', 'owner', ]
 
-admin.site.register(Domain)
+admin.site.register(Domain, DomainAdmin)
 admin.site.register(RecordType)
 admin.site.register(Record)
 admin.site.register(Nameserver)
