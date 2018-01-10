42537661cf149b531c0d1659858336c0c41bb828
Alex Conrey <alex@conrey.us>
2018-01-09 23:10:23 -0600

    added list_records

--- domains/urls.py
+++ domains/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
 	url(r'^$', views.list_domains, name='index'),
 	url(r'^list/?$', views.list_domains, name='list_domains'),
        url(r'^create/?$', views.create_domain, name='create_domain'),
+       url(r'^records/list/?$', views.list_records, name='list_records'),
+       url(r'^records/create/?$', views.create_record, name='create_record'),
+
 
 ]
--- domains/views.py
+++ domains/views.py
@@ -4,8 +4,8 @@ from django.utils.translation import ugettext_lazy as _
 
 from django.shortcuts import render, HttpResponse, redirect
 from django import forms
-from .models import Domain
-from .forms import DomainForm
+from .models import Domain, Record
+from .forms import DomainForm, RecordForm
 
 
 
@@ -19,6 +19,11 @@ def list_domains(request):
 
 	return HttpResponse(render(request, 'domains/list_domains.html', context={'domains': domains}))	
 
+def list_records(request):
+    records = Record.objects.values()
+
+    return HttpResponse(render(request, 'domains/list_records.html', context={'records': records}))
+
 def create_domain(request):
         if request.method == 'POST':
             domain = Domain()
@@ -30,3 +35,15 @@ def create_domain(request):
         else:
             form = DomainForm()
         return HttpResponse(render(request, 'domains/create.html', {'form': form}))
+
+def create_record(request):
+    if request.method == 'POST':
+        record = Record()
+        form = RecordForm(request.POST, instance=record)
+        if form.is_valid():
+            if not Record.objects.filter(domain = form.cleaned_data['domain']).filter(name = form.cleaned_data['name']):
+                record.save()
+            return redirect('/domains/records/list')
+    else:
+        form = RecordForm()
+    return HttpResponse(render(request, 'domains/create.html', {'form': form}))
